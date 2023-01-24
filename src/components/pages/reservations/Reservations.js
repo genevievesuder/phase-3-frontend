0bad2f3a09bac0d856d2f89a3bc89c96df50e4dd
Ryan <sullivan050ryan@gmail.com>
2023-01-24 11:21:39 -0800

    remove extra stuff from reservations

--- src/components/pages/reservations/Reservations.js
+++ src/components/pages/reservations/Reservations.js
@@ -4,22 +4,11 @@ import { useState } from 'react';
 
 const Reservations = ({rooms}) => {
 
-    const [form, showForm] = useState(false)
-
-    
-    const handleShowForm = () => {
-        showForm(currentState => !currentState); 
-    }
-    
     const mappedRooms = rooms.map(room => <Rooms {...room} key={room.id} />);
 
 
     return(
        <div className="reservations">
-            {/* <button className="show-form-btn" onClick={handleShowForm}>Create a Reservation</button>
-            {form ? (
-            <ResForm />
-            ) : null} */}
             <ResForm />
             {mappedRooms}
        </div>
