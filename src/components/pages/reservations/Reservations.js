2fe35bfacfcb8e0cfb3c8111ddd7386b4097eb80
Ryan <sullivan050ryan@gmail.com>
2023-01-25 20:39:58 -0800

    add toggle room picture/details function

--- src/components/pages/reservations/Reservations.js
+++ src/components/pages/reservations/Reservations.js
@@ -5,21 +5,18 @@ import { useState } from 'react';
 const Reservations = ({data}) => {
 
     const mappedRooms = data.map(room => <Rooms 
-        bed_size = {room.bed_size} room_name= {room.room_name}
+        bed_size = {room.bed_size} 
+        room_name= {room.room_name}
         image_url= {room.image_url}  
         key={room.id} 
         reservations={room.reservations}
         />);
 
-        const handleClick = e => {
-            console.log("working")
-            
-        }
+   
 
     return(
-       <div onClick = {handleClick} className="reservations">
-            <ResForm 
-                data={data}/>
+       <div className="reservations">
+            <ResForm data={data}/>
             {mappedRooms}
        </div>
     )
