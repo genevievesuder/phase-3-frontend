90b3b45f3fc748dd19d9d5d1329564aae74990d4
Ryan <sullivan050ryan@gmail.com>
2023-01-26 21:09:29 -0800

    add some styling

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
     
     return(
         <div className="home">
-            <h1>Welcome</h1>
+            <article className= 'message_to_guest'>Experience the perfect blend of comfort and convenience at our small hotel. Nestled in a quiet and peaceful neighborhood, our hotel offers a warm and welcoming atmosphere that will make you feel right at home.  </article>
         </div>
     )
 }
